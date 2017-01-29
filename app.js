119767fa1add56f6fd8bce5ca9b0be750370af28
patrice prosper <new>
2017-01-29 15:41:59 -0500

    database and materialize working

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const reload = require('reload');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
--- db/models/user.js
+++ db/models/user.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
-  var Users = sequelize.define('Users', {
+  var User = sequelize.define('User', {
     username: DataTypes.STRING(32),
     password: DataTypes.TEXT,
     email: DataTypes.STRING(128),
@@ -14,5 +14,5 @@ module.exports = function(sequelize, DataTypes) {
       }
     }
   });
-  return Users;
+  return User;
 };
